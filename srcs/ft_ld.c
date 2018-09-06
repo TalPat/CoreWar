2a4d50fae6ce6d65510709d64f35dfe912eda715
charl KING <cking@c3r3s4.wethinkcode.co.za>
2018-09-06 17:08:27 +0200

    ld added

--- srcs/ft_ld.c
+++ srcs/ft_ld.c
@@ -6,7 +6,7 @@
 /*   By: cking <cking@student.wethinkcode.co.za>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/05 16:10:31 by cking             #+#    #+#             */
-/*   Updated: 2018/09/06 16:07:14 by cking            ###   ########.fr       */
+/*   Updated: 2018/09/06 17:04:08 by cking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,24 @@
 void		ft_ld(t_cw *cw, t_pc *pc)
 {
 	int		*arr;
+	int		argsize;
+	char	*temp;
 
 	pc->cr = 5;
 	if (ft_verify_eb(cw, pc))
 	{
 		arr = ft_getparam(cw->mem[pc->index + 1]);
+		if (arr[0] == T_DIR)
+			argsize = DIR_SIZE;
+		else
+			argsize = IND_SIZE;
+
+		temp = ft_strsub(cw->mem, pc->index + 2, argsize);
+		pc->index = pc->index + 2 + argsize;
+		pc->registers[cw->mem[pc->index]] = temp;
+		pc->index++;
+		free(temp);
 	}
 	else
-		pc->index++;
+		pc->index += 2;
 }
