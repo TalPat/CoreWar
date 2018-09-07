5419f193f1c5147807d358365462f77f3c1c1833
charl KING <cking@c1r2s4.wethinkcode.co.za>
2018-09-07 13:36:11 +0200

    print bits

--- srcs/print_bits.c
+++ srcs/print_bits.c
@@ -1,18 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   print_bits.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cking <cking@student.wethinkcode.co.za>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/07 13:19:21 by cking             #+#    #+#             */
-/*   Updated: 2018/09/07 13:20:11 by cking            ###   ########.fr       */
+/*   Created: 2018/09/07 13:12:07 by cking             #+#    #+#             */
+/*   Updated: 2018/09/07 13:36:02 by cking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/cw.h"
 
-int		main(void)
+void	print_bits(t_cw *cw, int index, int n)
 {
-	char test[4];
-	test[01] = 'a';
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		ft_putstr(ft_itoa_base(cw->mem[i], 16));
+		ft_puchar(' ');
+		i++;
+	}
 }
