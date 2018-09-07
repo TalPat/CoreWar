0b9973000fb92190d40d8a58ca168758559100cc
jasmine DE-AGRELA <jde-agr@c1r2s3.wethinkcode.co.za>
2018-09-07 17:00:55 +0200

    need to match registers when adding

--- srcs/ft_add.c
+++ srcs/ft_add.c
@@ -11,10 +11,13 @@ void	ft_add(t_cw *cw, t_pc *pc)
 	num = 0;
 	if (ft_verify_eb(cw, pc)) //valid eb
 	{
-		newidx += 2;
+		newidx += 2; //need to work on identifying registers!!
 		//pc->registers[newidx + 2] = pc->registers[newidx] + pc->registers[newidx + 1];
-		num = (ft_atoi_base((char*)pc->registers[newidx], 10) + ft_atoi_base((char*)pc->registers[newidx + 1], 10));
+		num = (ft_atoi_base((char*)pc->registers[newidx - 1], 10) + ft_atoi_base((char*)pc->registers[newidx], 10));
+		ft_putnbr(num);
+		ft_putchar('\n');
 		pc->registers[newidx + 2] = (unsigned char*)ft_itoa_base(num, 16);
+		ft_putstr((char*)pc->registers[newidx + 2]);
 	}
 	pc->index++;
 }
@@ -37,17 +40,13 @@ int main(void)
 	pc->index = 0;
 	
 	ft_print_bits(cw, 0, 5);
-	ft_putstr("hi\n");
-	pc->registers[0][0] = (unsigned char)ft_strdup("1");
-	ft_putstr("hi\n");
-	pc->registers[0][1] = (unsigned char)ft_strdup("2");
-	ft_putstr("hi\n");
-	pc->registers[1] = 0;
-	ft_putstr("hi\n");
-	pc->registers[2][0] = (unsigned char)ft_strdup("3");
-	pc->registers[2][1] = (unsigned char)ft_strdup("0");
-	pc->registers[3] = 0;
-	ft_putstr("hi\n");
+	pc->registers[1] = (unsigned char*)"12";
+	pc->registers[2] = (unsigned char*)"-2";
+	pc->registers[3] = (unsigned char*)"30";
+	//pc->registers[2][0] = (unsigned char)ft_strdup("3");
+	//pc->registers[2][1] = (unsigned char)ft_strdup("0");
+	ft_putchar('\n');
 	ft_add(cw, pc);
+	ft_putstr((char*)pc->registers[4]);
 	//ft_ld(cw, pc);
 }
