a0ac2bc43057c893ab7cdb988ac2b155b162693b
charl KING <cking@c1r2s4.wethinkcode.co.za>
2018-09-07 12:29:26 +0200

    just ld things

--- srcs/ft_ld.c
+++ srcs/ft_ld.c
@@ -6,33 +6,56 @@
 /*   By: cking <cking@student.wethinkcode.co.za>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/05 16:10:31 by cking             #+#    #+#             */
-/*   Updated: 2018/09/06 17:04:08 by cking            ###   ########.fr       */
+/*   Updated: 2018/09/07 12:29:18 by cking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cw.h"
+#include "../includes/cw.h"
 
 void		ft_ld(t_cw *cw, t_pc *pc)
 {
-	int		*arr;
-	int		argsize;
-	char	*temp;
+	int				*arr;
 
 	pc->cr = 5;
 	if (ft_verify_eb(cw, pc))
 	{
 		arr = ft_getparam(cw->mem[pc->index + 1]);
 		if (arr[0] == T_DIR)
-			argsize = DIR_SIZE;
+		{
+			ft_memcpy(pc->registers[cw->mem[pc->index + 2 + DIR_SIZE]],
+			cw->mem[pc->index + 2], DIR_SIZE);
+			pc->index = pc->index + 2 + DIR_SIZE;
+		}
 		else
-			argsize = IND_SIZE;
+		{
 
-		temp = ft_strsub(cw->mem, pc->index + 2, argsize);
-		pc->index = pc->index + 2 + argsize;
-		pc->registers[cw->mem[pc->index]] = temp;
-		pc->index++;
-		free(temp);
+			pc->index = pc->index + 2 + IND_SIZE;
+		}
 	}
 	else
 		pc->index += 2;
+}
+
+int		main(void)
+{
+	t_cw			*cw;
+	t_pc			*pc;
+
+	cw = (t_cw *)malloc(sizeof(t_cw));
+	ft_inittable(cw);
+	cw->mem = (unsigned char *)malloc(sizeof(unsigned char) * 7);
+	cw->mem[0] = 2;
+	cw->mem[1] = 144;
+	cw->mem[2] = 0;
+	cw->mem[3] = 0;
+	cw->mem[4] = 0;
+	cw->mem[5] = 'B';
+	cw->mem[6] = 2;
+	pc->index = 0;
+	pc->registers[0] = 0;
+	pc->registers[1] = 0;
+	pc->registers[2] = 0;
+	pc->registers[3] = 0;
+	ft_ld(cw, pc);
+	ft_putnbr(pc->registers[2]);
 }
