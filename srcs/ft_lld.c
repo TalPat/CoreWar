46a9ed813066422ca4128c47010f41f619452f07
charl KING <cking@c3r2s5.wethinkcode.co.za>
2018-09-11 16:33:29 +0200

    lld done, I think

--- srcs/ft_lld.c
+++ srcs/ft_lld.c
@@ -1,37 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ld.c                                            :+:      :+:    :+:   */
+/*   ft_lld.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cking <cking@student.wethinkcode.co.za>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/05 16:10:31 by cking             #+#    #+#             */
-/*   Updated: 2018/09/11 12:05:25 by cking            ###   ########.fr       */
+/*   Created: 2018/09/11 16:03:07 by cking             #+#    #+#             */
+/*   Updated: 2018/09/11 16:32:50 by cking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cw.h"
 
-void		ft_lddir(t_cw *cw, t_pc *pc)
+void		ft_llddir(t_cw *cw, t_pc *pc)
 {
 	int i;
 	int regnum;
+	int check;
 
 	i = 0;
 	regnum = cw->mem[pc->index + 2 + DIR_SIZE];
 	while (i < REG_SIZE)
 	{
 		pc->registers[regnum][i] = cw->mem[pc->index + 2 + i];
+		if (cw->mem[pc->index + 2 + i])
+			check++;
 		i++;
 	}
 	pc->index = pc->index + 2 + DIR_SIZE;
+	if (!check)
+		pc->carry = '1';
+	else
+		pc->carry = NULL;
 }
 
-void		ft_ldind(t_cw *cw, t_pc *pc)
+void		ft_lldind(t_cw *cw, t_pc *pc)
 {
 	int regnum;
 	int i;
 	int offset;
+	int check;
 
 	regnum = cw->mem[pc->index + 2 + IND_SIZE];
 	i = 0;
@@ -39,11 +47,17 @@ void		ft_ldind(t_cw *cw, t_pc *pc)
 	while (i < REG_SIZE)
 	{
 		pc->registers[regnum][i] = cw->mem[pc->index + i + (offset % IDX_MOD)];
+		if (cw->mem[pc->index + i + (offset % IDX_MOD)])
+			check++;
 		i++;
+		if (!check)
+			pc->carry = '1';
+		else
+			pc->carry = NULL;
 	}
 }
 
-void		ft_ld(t_cw *cw, t_pc *pc)
+void		ft_lld(t_cw *cw, t_pc *pc)
 {
 	int		*arr;
 	int		i;
@@ -61,36 +75,3 @@ void		ft_ld(t_cw *cw, t_pc *pc)
 		pc->index += 2;
 	free(arr);
 }
-
-// int		main(void)
-// {
-// 	t_cw			*cw;
-// 	t_pc			*pc;
-// 	int				i;
-
-// 	cw = (t_cw *)malloc(sizeof(t_cw));
-// 	pc = (t_pc *)malloc(sizeof(t_pc));
-// 	ft_inittable(cw);
-// 	cw->mem = (unsigned char*)malloc(sizeof(unsigned char) * MEM_SIZE);
-// 	ft_bzero(cw->mem, MEM_SIZE);
-// 	cw->mem[0] = 2;
-// 	cw->mem[1] = 208;
-// 	cw->mem[2] = 255;
-// 	cw->mem[3] = 0;
-// 	cw->mem[4] = 2;
-// 	cw->mem[5] = 0;
-// 	cw->mem[6] = 0;
-// 	pc->index = 0;
-// 	ft_print_bits(cw, 0, 7);
-// 	pc->registers = (unsigned char **)malloc(sizeof(unsigned char *) * REG_NUMBER);
-// 	i = 0;
-// 	while (i < REG_NUMBER)
-// 	{
-// 		pc->registers[i] = (unsigned char *)malloc(sizeof(unsigned char) * REG_SIZE);
-// 		ft_bzero(pc->registers[i], REG_SIZE);
-// 		i++;
-// 	}
-// 	ft_ld(cw, pc);
-// 	ft_print_reg(pc);
-// 	ft_putnbr(ft_hextodec("ff00"));
-// }
--- test/mess.s
+++ test/mess.s
@@ -1,7 +1,4 @@
 .name "messaround"
 .comment "hmmm"
 
-ld 5, r2
-ld 2, r2
-
-  
+lld %0, r5
