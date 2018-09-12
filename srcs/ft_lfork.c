5ce0cc5e31284ab83338fb178b3218c5d4604a50
talon PATTERSON <tpatter@c3r3s3.wethinkcode.co.za>
2018-09-12 16:30:02 +0200

    FORK!

--- srcs/ft_lfork.c
+++ srcs/ft_lfork.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fork.c                                          :+:      :+:    :+:   */
+/*   ft_lfork.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpatter <tpatter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/10 20:14:25 by tpatter           #+#    #+#             */
-/*   Updated: 2018/09/12 16:26:32 by tpatter          ###   ########.fr       */
+/*   Created: 2018/09/12 16:27:06 by tpatter           #+#    #+#             */
+/*   Updated: 2018/09/12 16:29:13 by tpatter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ void	ft_fork(t_cw *cw, t_pc *pc)
 
 	pc->cr = cw->op_tab[11].ctc;
 	newpc = (t_pc*)malloc(sizeof(pc));
-	newpc->index = (pc->index + (ft_getdir(cw, pc->index + 1) % IDX_MOD))
-		% MEM_SIZE;
+	newpc->index = (pc->index + (ft_getdir(cw, pc->index + 1))) % MEM_SIZE;
 	newpc->idnbr = pc->idnbr;
 	newpc->cr = pc->cr;
 	newpc->carry = pc->carry;
@@ -31,7 +30,7 @@ void	ft_fork(t_cw *cw, t_pc *pc)
 	i = 0;
 	while (i < REG_NUMBER)
 	{
-		newpc->registers[i] = (unsigned char*)malloc(sizeof(char) * REG_SIZE);
+		newpc->registers[i] = (char*)malloc(sizeof(char) * REG_SIZE);
 		ft_memcpy(newpc->registers[i], pc->registers[i], REG_SIZE);
 		i++;
 	}
@@ -39,6 +38,8 @@ void	ft_fork(t_cw *cw, t_pc *pc)
 	newpclist->content = newpc;
 	newpclist->content_size = sizeof(t_pc);
 	ft_lstadd(&(cw->pclist), newpclist);
+	if (newpc->index == 0)
+		pc->carry = 1;
 	newidx = DIR_SIZE;
 	pc->index = (pc->index + newidx) % MEM_SIZE;
 }
