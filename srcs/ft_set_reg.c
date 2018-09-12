53b5ed3f07aceafc227c3285b4d2d188e5502be0
charl KING <cking@c3r3s4.wethinkcode.co.za>
2018-09-12 13:19:38 +0200

    set_reg done ?

--- srcs/ft_set_reg.c
+++ srcs/ft_set_reg.c
@@ -6,7 +6,7 @@
 /*   By: cking <cking@student.wethinkcode.co.za>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/12 12:18:15 by cking             #+#    #+#             */
-/*   Updated: 2018/09/12 12:52:25 by cking            ###   ########.fr       */
+/*   Updated: 2018/09/12 13:15:03 by cking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,26 @@ void		ft_set_reg_reg(t_pc *pc, int reg_src, int reg_dst)
 
 void		ft_set_reg_ind(t_cw *cw, t_pc *pc, int index, int reg_num)
 {
+	int i;
 
+	ft_bzero(pc->registers[reg_num], REG_SIZE);
+	i = IND_SIZE;
+	while (i)
+	{
+		pc->registers[reg_num][i - 1] = cw->mem[index + i - 1];
+		i--;
+	}
 }
 
-void		ft_set_reg_redir(t_cw *cw, t_pc *pc, int index, int reg_num)
+void		ft_set_reg_dir(t_cw *cw, t_pc *pc, int index, int reg_num)
 {
+	int i;
 
+	ft_bzero(pc->registers[reg_num], REG_SIZE);
+	i = DIR_SIZE;
+	while (i)
+	{
+		pc->registers[reg_num][i - 1] = cw->mem[index + i - 1];
+		i--;
+	}
 }
