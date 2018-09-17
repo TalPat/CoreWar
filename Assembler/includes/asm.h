efae0b3657b027cd51188509042e981661579d02
talon PATTERSON <tpatter@c3r3s6.wethinkcode.co.za>
2018-09-17 14:43:41 +0200

    minor additions

--- Assembler/includes/asm.h
+++ Assembler/includes/asm.h
@@ -1,14 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   asm.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpatter <tpatter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/17 13:49:13 by tpatter           #+#    #+#             */
-/*   Updated: 2018/09/17 13:50:53 by tpatter          ###   ########.fr       */
+/*   Created: 2018/09/17 13:50:04 by tpatter           #+#    #+#             */
+/*   Updated: 2018/09/17 14:02:52 by tpatter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "asm.h"
+#include "libft.h"
+#include <stdlib.h>
+#include "op.h"
 
+#ifndef ASM_H
+# define ASM_H
+
+typedef struct	s_label
+{
+	int			index;
+	char		*name;
+}				t_label;
+
+#endif
