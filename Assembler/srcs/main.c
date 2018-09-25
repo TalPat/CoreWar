ae9a7286de6da29936d74e3d4dad833cf0193e8e
Charl King <charlwking@gmail.com>
2018-09-25 14:14:13 +0200

    trim not working yet

--- Assembler/srcs/main.c
+++ Assembler/srcs/main.c
@@ -10,7 +10,83 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/asm.h"
+//#include "../includes/asm.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+int		ft_isspace(int c)
+{
+	if (c == '\t' || c == '\n' || c == '\v' ||
+			c == '\f' || c == '\r' || c == ' ')
+		return (1);
+	return (0);
+}
+
+int	ft_islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
+
+int	ft_isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
+
+int	ft_isalpha(int c)
+{
+	if (ft_isupper(c) || ft_islower(c))
+		return (1);
+	return (0);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
+int	ft_isalnum(int c)
+{
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	return (0);
+}
+
+int	ft_strlen(const char *s)
+{
+	int i;
+
+	i = -1;
+	while (*(s + ++i))
+		;
+	return (i);
+}
+
+int		ft_wcount(const char *str, char c)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 0;
+	if (!str)
+		return (0);
+	while (*(str + i))
+	{
+		while (*(str + i) == c)
+			i++;
+		if (*(str + i) != c && *(str + i))
+			count++;
+		while (*(str + i) != c && *(str + i))
+			i++;
+	}
+	return (count);
+}
 
 int		spacecount(char *str)
 {
@@ -21,12 +97,31 @@ int		spacecount(char *str)
 	count = 0;
 	while(str[i])
 	{
-		if(str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		if(ft_isspace(str[i]))
 			count++;
 		i++;
 	}
 	return (count);
 }
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		ft_putchar(str[index]);
+		index++;
+	}
+}
+
+
 
 char	*trim(char *str)
 {
@@ -36,18 +131,18 @@ char	*trim(char *str)
 	int		j; //incrementor in old string
 	int		wcount;
 
-	newlen = ft_strlen(str) - (spacecount(str) - (ft_wcount(str) - 1)) + 1;
+	newlen = ft_strlen(str) - (spacecount(str) - (ft_wcount(str, ' ') - 1)) + 1;
 	out = (char *)malloc(sizeof(char) * newlen);
 	i = 0;
 	j = 0;
-	wcount = ft_wcount(str);
+	wcount = ft_wcount(str, ' ');
 	while (wcount)
 	{
 		while(ft_isspace(str[i]))
 			i++;
-		while(ft_isalnum)(str[i])
+		while(ft_isalnum(str[i]))
 		{
-			out[j] = str[i]
+			out[j] = str[i];
 			i++;
 			j++;
 		}
@@ -62,6 +157,7 @@ char	*trim(char *str)
 int		main(int ac, char **av)
 {	
 	char *str = "   test  test  \ntest";
-	char *trim = ft_strtrim(str);
-	ft_putstr(trim);
+	char *out = trim(str);
+	ft_putstr(out);
+	ft_putchar('\n');
 }
