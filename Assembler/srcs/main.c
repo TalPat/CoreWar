41a1f1c4e891efc04f10fedbe8e28b72b23d3259
Charl King <charlwking@gmail.com>
2018-09-25 16:38:48 +0200

    .s read

--- Assembler/srcs/main.c
+++ Assembler/srcs/main.c
@@ -14,155 +14,66 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
 
-int		ft_isspace(int c)
+int			ft_file_size(char *file) // Count bytes to malloc for
 {
-	if (c == '\t' || c == '\n' || c == '\v' ||
-			c == '\f' || c == '\r' || c == ' ')
-		return (1);
-	return (0);
-}
+	int				i;
+	int				fd;
+	int				ret;
+	unsigned char	c;
 
-int	ft_islower(int c)
-{
-	if (c >= 97 && c <= 122)
-		return (1);
-	return (0);
-}
-
-int	ft_isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-		return (1);
-	return (0);
-}
-
-int	ft_isalpha(int c)
-{
-	if (ft_isupper(c) || ft_islower(c))
-		return (1);
-	return (0);
-}
-
-int	ft_isdigit(int c)
-{
-	if (c >= 48 && c <= 57)
-		return (1);
-	return (0);
-}
-
-int	ft_isalnum(int c)
-{
-	if (ft_isalpha(c) || ft_isdigit(c))
-		return (1);
-	return (0);
-}
-
-int	ft_strlen(const char *s)
-{
-	int i;
-
-	i = -1;
-	while (*(s + ++i))
-		;
+	i = 0;
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	while (1)
+	{
+		ret = read(fd, &c, 1);
+		if (ret == 1)
+			i++;		
+		if (ret < 1)
+			break ;
+	}
+	close(fd);
 	return (i);
 }
 
-int		ft_wcount(const char *str, char c)
+char *ft_store_info(char *file, int size) // Store player file info into temp char **
 {
-	int	count;
-	int	i;
+	int fd;
+	int i;
+	int ret;
+	unsigned char c;
+	unsigned char *str;
 
+	str = (unsigned char *)malloc(sizeof(unsigned char) * (size));
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
 	i = 0;
-	count = 0;
-	if (!str)
-		return (0);
-	while (*(str + i))
+	while (i < size)
 	{
-		while (*(str + i) == c)
-			i++;
-		if (*(str + i) != c && *(str + i))
-			count++;
-		while (*(str + i) != c && *(str + i))
-			i++;
+		ret = read(fd, &c, 1);
+		if (ret == 1)
+			str[i++] = c;
+		if (ret < 1)
+			break;
 	}
-	return (count);
-}
-
-int		spacecount(char *str)
-{
-	int		count;
-	int		i;
-
-	i = 0;
-	count = 0;
-	while(str[i])
-	{
-		if(ft_isspace(str[i]))
-			count++;
-		i++;
-	}
-	return (count);
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	int index;
-
-	index = 0;
-	while (str[index] != '\0')
-	{
-		ft_putchar(str[index]);
-		index++;
-	}
-}
-
-
-
-char	*trim(char *str)
-{
-	int		newlen;
-	char	*out;
-	int		i; //incrementor in new string
-	int		j; //incrementor in old string
-	int		wcount;
-
-	newlen = ft_strlen(str) - (spacecount(str) - (ft_wcount(str, ' ') - 1)) + 1;
-	out = (char *)malloc(sizeof(char) * newlen);
-	i = 0;
-	j = 0;
-	wcount = ft_wcount(str, ' ');
-	while (wcount)
-	{
-		while(ft_isspace(str[i]))
-			i++;
-		while(ft_isalnum(str[i]))
-		{
-			out[j] = str[i];
-			printf("str[%d]:%c\n", i, str[i]);
-			i++;
-			j++;
-		}
-		wcount--;
-		while(ft_isspace(str[i]))
-			i++;
-		if (wcount)
-			out[j++] = ' ';
-	}
-	out[j] = '\0';
-	return (out);
+	str[i] = '\0';
+	close(fd);
+	return (str);
 }
 
 int		main(int ac, char **av)
-{	
-	char *str = "   test  test  \ntest";
-	char *out = trim(str);
-	ft_putstr(out);
-	ft_putchar('#');
-	ft_putchar('\n');
+{
+	int fd = open("test.s", O_RDONLY);
+	int size = ft_file_size("test.s");
+	char *info;
+	char **args;
+	info = ft_store_info("test.s", size);
+	args = ft_strsplit(info, '\n');
+
 }
